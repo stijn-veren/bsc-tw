eed5db48c16d47ba00a4bda385006a041f8cb5a6
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-28 00:29:12 +0300

    Fix grid import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query'
 // Components
 import Drawer from '@material-ui/core/Drawer'
 import LinearProgress from '@material-ui/core/LinearProgress'
-import Grid from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/Badge'
 // Styles
