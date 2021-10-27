fb6a290ee41ea711a1fc602b49ecb13d11195229
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-27 23:45:02 +0300

    Add prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useState } from 'react'
+import { useQuery } from 'react-query'
 // Components
-import Drawer from "@material-ui/core/Drawer";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Grid from "@material-ui/core";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import Badge from "@material-ui/core/Badge";
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
 // Styles
-import { Wrapper } from "./App.styles";
+import { Wrapper } from './App.styles'
 
 const App = () => {
-  return <div className="App">Start</div>;
-};
+  return <div className="App">Start</div>
+}
 
-export default App;
+export default App
