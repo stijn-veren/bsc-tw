a1af009535a4090d4dbb4ee1030c3a228c7c6f53
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-28 00:29:41 +0300

    Add item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useQuery } from 'react-query'
 // Components
+import Item from './Item/Item'
 import Drawer from '@material-ui/core/Drawer'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid'
@@ -32,14 +33,24 @@ const App = () => {
 
   const getTotalItems = () => null
 
-  const handleAddToCart = () => null
+  const handleAddToCart = (clickedItem: CartItemType) => null
 
   const handleRemoveFromCart = () => null
 
   if (isLoading) return <LinearProgress />
   if (error) return <div>Something went wrong ...</div>
 
-  return <div className="App">Start</div>
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  )
 }
 
 export default App
