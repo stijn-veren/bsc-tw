089284ff53cadf57583c2716530c19b7817a1ee9
Kostiantyn Verenych <kverenych@atlassian.com>
2021-10-28 04:45:37 +0300

    Add styled button

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Grid from '@material-ui/core/Grid'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/Badge'
 // Styles
-import { Wrapper } from './App.styles'
+import { Wrapper, StyledButton } from './App.styles'
 // Types
 export type CartItemType = {
   id: number
@@ -33,7 +33,7 @@ const App = () => {
 
   console.log(data)
 
-  const getTotalItems = () => null
+  const getTotalItems = (items: CartItemType[]) => null
 
   const handleAddToCart = (clickedItem: CartItemType) => null
 
@@ -47,6 +47,11 @@ const App = () => {
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
         Cart goes here
       </Drawer>
+      <StyledButton onClick={()=> setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
